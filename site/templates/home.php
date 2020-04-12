3dd28033876046e8c600ea10476a5245eb4b3fb8
rpetroudis <raydaniel.petroudis@gmail.com>
2020-04-12 15:15:18 +0200

    Update 3 - test

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,20 +1,5 @@
+<?php snippet('header') ?>
 
-<html lang="en">
-<head>
-
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  
-  <link rel="stylesheet" href="assets/css/costum1.css" type="text/css">
-
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700&display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great&display=swap" rel="stylesheet">
-  <link rel="icon" href="<?= $site->image()->url() ?>">
-
-  <title><?= $site->title() ?> | <?= $page->title() ?></title>
-
-</head>
 
 <main>
   <div class="row align-items-center">
@@ -24,7 +9,7 @@
       <a href="<?= $site->page('projects') ?>" class="btn btn-primary" role="button" aria-pressed="true">View my work</a>
       <a href="<?= $site->page('about') ?>" class="btn btn-outline-primary" role="button" aria-pressed="true">More about me</a>
     </div>
-    <div class="col-12 col-lg-6">
+    <div class="col-12 col-lg-6" style:>
       <img src="<?= $page->image()->url(); ?>" class="img-fluid" alt="<?= $page->image()->title(); ?>" />
     </div>
   </div>
