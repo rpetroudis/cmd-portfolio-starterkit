33a845638ae4ebfd998af31b4a3adf876f4c2381
rpetroudis <raydaniel.petroudis@gmail.com>
2020-04-12 04:29:23 +0200

    Update 3

--- site/templates/home.php
+++ site/templates/home.php
@@ -28,27 +28,6 @@
       <img src="<?= $page->image()->url(); ?>" class="img-fluid" alt="<?= $page->image()->title(); ?>" />
     </div>
   </div>
-  <?php
-  if ($projectsPage = page('projects')): ?>
-  <div class="row border-top">
-    <div class="col-12">
-      <h2 class="float-left">My work</h2>
-      <a class="float-right" href="<?= $site->page('projects') ?>">View all projects &#8594;</a>
-    </div>
-    <?php foreach ($projectsPage->children()->listed()->paginate(3) as $project): ?>
-    <div class="col-12 col-lg-4">
-      <div class="card h-100">
-      <img src="<?= $project->images()->findBy("template", "thumbnail")->url(); ?>" class="card-img-top" alt="thumbnail" />
-      <div class="card-body">
-        <h4 class="card-title"><a href="<?= $project->url() ?>"><?= $project->title() ?></a></h4>
-        <p class="card-text"><?= $project->roles() ?></p>
-        <a href="<?= $project->url() ?>">Read more &#8594;</a>
-      </div>
-    </div>
-    </div>
-    <?php endforeach ?>
-  </div>
-  <?php endif ?>
 </main>
 
 <?php snippet('footer') ?>
