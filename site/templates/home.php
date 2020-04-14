7b1177e8465b902fc7c96e757e9fd87cdfd6a744
rpetroudis <raydaniel.petroudis@gmail.com>
2020-04-14 18:31:55 +0200

    Update 6
    New link video + png logo

--- site/templates/home.php
+++ site/templates/home.php
@@ -23,7 +23,7 @@
   <div class="back">
   <div class="row align-items-center">
     <div class="col-12 col-lg-6">
-    <img src="/media/img/NEW LOGO-161.png" width="75px" height="75px" alt="<?= $site->image()->alt() ?>" class="square"/>
+    <img src="<?= $site->image()->url() ?>" width="75px" height="75px" alt="<?= $site->image()->alt() ?>" class="square"/>
       <h1><?= $page->introductiontitle() ?></h1>
       <p class="lead"><?= $page->introductiontext() ?></p>
       <a href="<?= $site->page('projects') ?>" class="btn btn-primary" role="button" aria-pressed="true">View my work</a>
