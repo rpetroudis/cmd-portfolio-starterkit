ab100c9a995a4406b62a5a7cd66544656e878f49
rpetroudis <raydaniel.petroudis@gmail.com>
2020-04-27 00:55:42 +0200

    Update 17
    Changed footer version 1

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -4,21 +4,25 @@
         <h2 id="social">Feel free to contact me</h2>
       </div>
     </div>
-    <div class="row">
+    <div class="row-one">
       <div class="col d-flex justify-content-center">
         <ul class="nav">
-          <?php foreach ($site->social()->toStructure() as $social): ?>
+
           <li class="nav-item">
-            <a href="<?= $social->url() ?>" class="nav-link" target="_blank"><?= $social->platform() ?></a>
+            <a class="nav-link" href="https://www.linkedin.com/in/raydaniel-petroudis-818814137/" target="_blank"> <img src="/content/2_linkedin.png" height="50px" alt="LinkedIn"></a>
           </li>
-          <?php endforeach ?>
+
           <li class="nav-item">
-            <a class="nav-link" href="mailto:<?= $site->email() ?>" target="_blank">E-mail</a>
+            <a class="nav-link" href="https://instagram.com/raydanielpetroudis" target="_blank"> <img src="/content/3_instagram.png" height="50px" alt="Instagram"></a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="mailto:<?= $site->email() ?>" target="_blank"> <img src="/content/4_gmail.png" height="50px" alt="Gmail"></a>
           </li>
         </ul>
       </div>
     </div>
-    <div class="row border-top">
+    <div class="row-two">
       <div class="col d-flex justify-content-center">
         <a href="<?= url() ?>">&copy; <?= $site->title() ?> <?= date('Y') ?></a>
       </div>
@@ -27,9 +31,12 @@
 
 </div>
 
+
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="assets/css/costum1.css" type="text/css">
 
 </body>
 </html>
