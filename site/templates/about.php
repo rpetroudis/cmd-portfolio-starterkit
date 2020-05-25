b70ed428937f4e7118898c33585d9c37fc707f18
rpetroudis <raydaniel.petroudis@gmail.com>
2020-05-25 02:25:54 +0200

    test 15
    about - removed blocks

--- site/templates/about.php
+++ site/templates/about.php
@@ -8,7 +8,7 @@
 <div class="row">
   <div class="col-12 col-lg-9">
     <?php snippet('intro') ?>
-    <?= $page->blocks()->toData()->toJson()->html() ?>
+    <?= $page->toData()->toJson()->html() ?>
   </div>
   <div class="col-12 col-lg-3">
     <div class= 'resume'> <h2>Resume</h2> </div>
