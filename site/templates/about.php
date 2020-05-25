a2807aec536a3c2b64faf1d4ff8f9a11df212ad3
rpetroudis <raydaniel.petroudis@gmail.com>
2020-05-25 03:02:31 +0200

    test 19
    about - json - pluck - text

--- site/templates/about.php
+++ site/templates/about.php
@@ -8,7 +8,7 @@
 <div class="row">
   <div class="col-12 col-lg-9">
     <?php snippet('intro') ?>
-    <?= $page->pluck('content')->html() ?>
+    <?= $page->toJson()->pluck('content')->text()->html() ?>
   </div>
   <div class="col-12 col-lg-3">
     <div class= 'resume'> <h2>Resume</h2> </div>
