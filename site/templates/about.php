5fd5e565df9cf543221c5b6d601deeb36a3e95c0
rpetroudis <raydaniel.petroudis@gmail.com>
2020-05-25 03:27:41 +0200

    test 27
    about - removed html

--- site/templates/about.php
+++ site/templates/about.php
@@ -8,7 +8,7 @@
 <div class="row">
   <div class="col-12 col-lg-9">
     <?php snippet('intro') ?>
-    <?= $page->toData()->toString()->html() ?>
+    <?= $page->toData()->toString()?>
   </div>
   <div class="col-12 col-lg-3">
     <div class= 'resume'> <h2>Resume</h2> </div>
