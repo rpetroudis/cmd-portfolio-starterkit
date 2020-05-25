0fe5e62da0211a3cf9206968cdfab430cf07d957
rpetroudis <raydaniel.petroudis@gmail.com>
2020-05-25 03:22:54 +0200

    test 25
    about - todata tostring

--- site/templates/about.php
+++ site/templates/about.php
@@ -8,7 +8,7 @@
 <div class="row">
   <div class="col-12 col-lg-9">
     <?php snippet('intro') ?>
-    <?= $page->text()->blocks()->html() ?>
+    <?= $page->toData()->toString()->blocks()->html() ?>
   </div>
   <div class="col-12 col-lg-3">
     <div class= 'resume'> <h2>Resume</h2> </div>
