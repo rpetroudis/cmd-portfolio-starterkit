6c4be0938be999cf57e2dc41575fc4d76f3dadc0
rpetroudis <raydaniel.petroudis@gmail.com>
2020-05-25 03:05:39 +0200

    test 20
    about - removed json-pluck

--- site/templates/about.php
+++ site/templates/about.php
@@ -8,7 +8,7 @@
 <div class="row">
   <div class="col-12 col-lg-9">
     <?php snippet('intro') ?>
-    <?= $page->toJson()->pluck('content')->text()->block()->html() ?>
+    <?= $page->text()->block()->html() ?>
   </div>
   <div class="col-12 col-lg-3">
     <div class= 'resume'> <h2>Resume</h2> </div>
