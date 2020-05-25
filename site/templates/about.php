1e43d4ab359a5a4d076b8c5fefac7a5be65dffe6
rpetroudis <raydaniel.petroudis@gmail.com>
2020-05-25 03:12:11 +0200

    test 21
    about - data-pluck-page-text

--- site/templates/about.php
+++ site/templates/about.php
@@ -8,7 +8,7 @@
 <div class="row">
   <div class="col-12 col-lg-9">
     <?php snippet('intro') ?>
-    <?= $page->text()->block()->html() ?>
+    <?= $page->toData()->pluck('content')->or($page)->text()->html() ?>
   </div>
   <div class="col-12 col-lg-3">
     <div class= 'resume'> <h2>Resume</h2> </div>
