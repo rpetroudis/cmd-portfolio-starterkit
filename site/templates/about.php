bfda78b0d53247af95d545b6f405f9225e803fc2
rpetroudis <raydaniel.petroudis@gmail.com>
2020-05-25 02:20:08 +0200

    test 13
    about - todata -tojson -html

--- site/templates/about.php
+++ site/templates/about.php
@@ -8,7 +8,7 @@
 <div class="row">
   <div class="col-12 col-lg-9">
     <?php snippet('intro') ?>
-    <?= $page->text()->kirbytext() ?>
+    <?= $page->text()->blocks()->toData()->toJson()->html() ?>
   </div>
   <div class="col-12 col-lg-3">
     <div class= 'resume'> <h2>Resume</h2> </div>
